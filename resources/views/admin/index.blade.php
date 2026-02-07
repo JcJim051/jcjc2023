6c3bf9185f3e90291bcae5dc7fad317b6bcc0d4a
Jonathan <jonathan.c.jimenez@gmail.com>
2026-02-07 10:46:25 -0500

    oculatar avance de transmision

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -56,7 +56,7 @@
             </div>
 
             <div class="mt-3 row">
-                <div class="col-3">
+                <div class="col-12">
                     <div class="small-box bg-gradient-warning">
                         <div class="inner">
                             <h3>{{ $tmc }}</h3>
@@ -73,7 +73,7 @@
                     </div>
                 </div>
 
-                <div class="col-3">
+                {{-- <div class="col-3">
                     <div class="small-box bg-info">
                         <div class="inner">
                             <h3>{{ $tde }}</h3>
@@ -108,7 +108,7 @@
                         <div class="icon"><i class="fas fa-user-check"></i></div>
                     </div>
                 </div>
-            </div>
+            </div> --}}
 
         {{-- ===================== ESCRUTADOR ===================== --}}
         @elseif ($rol == "Esctrutador")
